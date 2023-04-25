55f568d30f7c24a0178b38a0eb2cb62588222926
Alpha870 <omar1987perez@gmail.com>
2023-04-25 20:22:01 +0200

    modificando estilos Slider

--- client/components/Carousel/MySlider.jsx
+++ client/components/Carousel/MySlider.jsx
@@ -4,46 +4,21 @@ import Carousel from "react-bootstrap/Carousel";
 const MySlider = ({ img1, img2, img3, img4, img5 }) => {
   return (
     <>
-      <Carousel>
+      <Carousel style={{ width: "100%" }}>
         <Carousel.Item interval={5000}>
-          <img
-            className="img-design"
-            src={img1}
-            alt="First slide"
-            style={{ height: "700px", width: "100%" }}
-          />
+          <img className="img-design" src={img1} alt="First slide" />
         </Carousel.Item>
         <Carousel.Item interval={5000}>
-          <img
-            className="img-design"
-            src={img2}
-            alt="Second slide"
-            style={{ height: "700px", width: "100%" }}
-          />
+          <img className="img-design" src={img2} alt="Second slide" />
         </Carousel.Item>
         <Carousel.Item interval={5000}>
-          <img
-            className="img-design"
-            src={img3}
-            alt="Third slide"
-            style={{ height: "700px", width: "100%" }}
-          />
+          <img className="img-design" src={img3} alt="Third slide" />
         </Carousel.Item>
         <Carousel.Item interval={5000}>
-          <img
-            className="img-design"
-            src={img4}
-            alt="Third slide"
-            style={{ height: "700px", width: "100%" }}
-          />
+          <img className="img-design" src={img4} alt="Third slide" />
         </Carousel.Item>
         <Carousel.Item interval={5000}>
-          <img
-            className="img-design"
-            src={img5}
-            alt="Third slide"
-            style={{ height: "700px", width: "100%" }}
-          />
+          <img className="img-design" src={img5} alt="Third slide" />
         </Carousel.Item>
       </Carousel>
     </>
